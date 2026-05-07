8b2b482fc765eaccaa7f39d4b0c2cfae166da773
fakhrulhasan99 <fakhrulhasan.faf1@gmail.com>
2026-05-07 15:15:26 +0600

    added blog-1

---
+++ blog-1.md
@@ -0,0 +1,29 @@
+# BLOG-1
+
+## Why is any labeled a "type safety hole," and why is unknown the safer choice for handling unpredictable data? Explain the concept of type narrowing.
+
+As we know Typescript is very similar to Javascript except it is more sensitive to data types and offers some extra syntaxes. Unlike JS it gives us error on the first place when we apply a wrong property to something which doesn't have that property even before we run the code.
+
+### any
+The `any` type completely disables Typescript's type cheching method. Actually when we declare the type of a data as `any` Typescript starts to rely on us. Even invalid operations get compiled successfully. Such as:
+
+```tsx
+let value: any = "Hello";
+
+console.log(value.toFixed(2));
+```
+
+This gives an error when we run the code. But doesn't warn us while coding because the type `any` prevents Typescript to check the type of a value.
+
+### unknown
+The `unknown` type also represents data which are unpredictable but in this case Typescript forces us to check the data type before using it. Such as:
+
+```tsx
+let value: unknown = "Hello";
+
+value.toUpperCase();
+```
+
+Gives an error immediately.
+
+This is the main reason why `unknown` is safer choice that `any` for handling unpredictable data.
--- solutions.ts
+++ solutions.ts
@@ -2,7 +2,7 @@
 // Solution 1
 
 function filterEvenNumbers(numbers: number[]) {
-    return numbers.filter(num => num % 2 === 0);
+  return numbers.filter(num => num % 2 === 0);
 }
 // console.log(filterEvenNumbers([1, 2, 3, 4, 5, 6]));
 
@@ -10,7 +10,7 @@ function filterEvenNumbers(numbers: number[]) {
 // Solution 2
 
 function reverseString(input: string) {
-    return input.split('').reverse().join('');
+  return input.split('').reverse().join('');
 };
 // console.log(reverseString("typescript"));
 
@@ -20,11 +20,11 @@ function reverseString(input: string) {
 type StringOrNumber = string | number;
 
 function checkType(value: StringOrNumber) {
-    if (typeof value === "string") {
-        return "String";
-    } else {
-        return "Number";
-    }
+  if (typeof value === "string") {
+    return "String";
+  } else {
+    return "Number";
+  }
 }
 // console.log(checkType("Hello"));
 // console.log(checkType(42));
@@ -33,7 +33,7 @@ function checkType(value: StringOrNumber) {
 // Solution 4
 
 function getProperty<T>(obj: T, key: keyof T) {
-    return obj[key];
+  return obj[key];
 };
 // const user = { id: 1, name: "John Doe", age: 21 };
 // console.log(getProperty(user, "name"));
@@ -53,11 +53,11 @@ function toggleReadStatus(book: Book): Book & { isRead: boolean } {
     isRead: true
   };
 };
-const myBook = {
-  title: "TypeScript Guide",
-  author: "Jane Doe",
-  publishedYear: 2024
-};
+// const myBook = {
+//   title: "TypeScript Guide",
+//   author: "Jane Doe",
+//   publishedYear: 2024
+// };
 // console.log(toggleReadStatus(myBook));
 
 
@@ -88,11 +88,10 @@ class Student extends Person {
 // const student = new Student("Alice", 20, "A");
 // console.log(student.getDetails());
 
-
 // Solution 7
 
 function getIntersection(arr1: number[], arr2: number[]) {
   const arr3 = new Set(arr1);
   return arr2.filter(num => arr3.has(num));
 }
-console.log(getIntersection([1,2,3,4,5], [3,4,5,6,7]));
+// console.log(getIntersection([1,2,3,4,5], [3,4,5,6,7]));
